e7d50a3f5f6be75801cd382df4d69da0ad124d0e
NIKUSHA CHAVCHAVADZE <nikolozi.tchavtchavadze.1@btu.edu.ge>
2023-09-09 18:42:17 +0400

    added BookingDetailedDescription folder

---
+++ src/components/modules/booking/BookingDetailed/BookingDetailedDescription/BookingDetailedDescription.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./BookingDetailedDescription.stylesheet.css";
+
+const BookingDetailedDescription = () => {
+  return <div>BookingDetailedDescription</div>;
+};
+
+export default BookingDetailedDescription;
