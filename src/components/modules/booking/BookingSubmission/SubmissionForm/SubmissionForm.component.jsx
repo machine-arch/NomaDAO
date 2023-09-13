7d4d352cba48918385c98751036b7080d1fde420
NIKUSHA CHAVCHAVADZE <nikolozi.tchavtchavadze.1@btu.edu.ge>
2023-09-13 22:22:33 +0400

    SubmissionFormComponent

--- src/components/modules/booking/BookingSubmission/SubmissionForm/SubmissionForm.component.jsx
+++ src/components/modules/booking/BookingSubmission/SubmissionForm/SubmissionForm.component.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import "./SubmissionForm.stylesheet.css";
 
 const SubmissionForm = () => {
-  return <div>SubmissionForm</div>;
+  return (
+    <div className="submission__form__container">
+      <div className="submission__header">
+        <span className="blue__box"></span>
+        <h1>Booking submission</h1>
+      </div>
+      <div className="submission__form">
+        <div className="submission__form__left">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Address 1" />
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="Zip Code/Postal Code" />
+        </div>
+        <div className="submission__form__right">
+          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Address 2" />
+          <input type="text" placeholder="State/Province/Region" />
+          <input type="text" placeholder="Country" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SubmissionForm;
