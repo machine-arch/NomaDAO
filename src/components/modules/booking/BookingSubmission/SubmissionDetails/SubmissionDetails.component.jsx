fc7abfbaaa1abe5cc53e973083b537170d636581
NIKUSHA CHAVCHAVADZE <nikolozi.tchavtchavadze.1@btu.edu.ge>
2023-09-14 12:15:28 +0400

    added payment imgs, submissionpayment component

--- src/components/modules/booking/BookingSubmission/SubmissionDetails/SubmissionDetails.component.jsx
+++ src/components/modules/booking/BookingSubmission/SubmissionDetails/SubmissionDetails.component.jsx
@@ -2,7 +2,81 @@ import React from "react";
 import "./SubmissionDetails.stylesheet.css";
 
 const SubmissionDetails = () => {
-  return <div>SubmissionDetails</div>;
+  return (
+    <div className="submission__details__container">
+      <h1>Your submitted booking</h1>
+      <div className="submission__details__info">
+        <h1 className="submission__details__hotelname">
+          The Tower Plaza Hotel Dubai
+        </h1>
+        <hr className="submission__details__line" />
+        <h1 className="submission__details__vendor">
+          Vendor: The Tower Plaza Hotel Dubai
+        </h1>
+        <img
+          className="submission__details__image"
+          src="/src/assets/images/mock_apartment.jpeg"
+        />
+        <div className="submission__details__location">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="17"
+              viewBox="0 0 16 17"
+              fill="none"
+            >
+              <path
+                d="M7.99967 1.83325C5.41967 1.83325 3.33301 3.91992 3.33301 6.49992C3.33301 9.99992 7.99967 15.1666 7.99967 15.1666C7.99967 15.1666 12.6663 9.99992 12.6663 6.49992C12.6663 3.91992 10.5797 1.83325 7.99967 1.83325ZM7.99967 8.16658C7.55765 8.16658 7.13372 7.99099 6.82116 7.67843C6.5086 7.36587 6.33301 6.94195 6.33301 6.49992C6.33301 6.05789 6.5086 5.63397 6.82116 5.32141C7.13372 5.00885 7.55765 4.83325 7.99967 4.83325C8.4417 4.83325 8.86562 5.00885 9.17819 5.32141C9.49075 5.63397 9.66634 6.05789 9.66634 6.49992C9.66634 6.94195 9.49075 7.36587 9.17819 7.67843C8.86562 7.99099 8.4417 8.16658 7.99967 8.16658Z"
+                fill="#3F3F3F"
+              />
+            </svg>
+            Location:
+          </div>
+          <h1>United Arab Emirates, Dubai</h1>
+        </div>
+        <h1 className="submission__payment__method">Select Payment Method</h1>
+        <hr className="submission__details__line" />
+        <div className="submission__reserve__info">
+          <div>
+            <h1>Check in</h1>
+            <h1>15/09/2023</h1>
+          </div>
+          <div>
+            <h1>Check out</h1>
+            <h1>05/10/2023</h1>
+          </div>
+          <div>
+            <h1>Nights</h1>
+            <h1>3</h1>
+          </div>
+        </div>
+        <hr className="submission__details__line" />
+        <div className="submission__reserve__info">
+          <div className="submission__coupon__box">
+            <input
+              className="submission__coupon"
+              type="text"
+              placeholder="Coupon code"
+            />
+            <button className="submission__coupon__apply">Apply</button>
+          </div>
+          <div>
+            <h1>Service Fee:</h1>
+            <h1>$0</h1>
+          </div>
+          <div>
+            <h1>Single Bed</h1>
+            <h1>38$</h1>
+          </div>
+          <div>
+            <h1>Total</h1>
+            <h1>$38</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SubmissionDetails;
--- src/components/modules/booking/BookingSubmission/SubmissionPayment/SubmissionPayment.component.jsx
+++ src/components/modules/booking/BookingSubmission/SubmissionPayment/SubmissionPayment.component.jsx
@@ -2,7 +2,49 @@ import React from "react";
 import "./SubmissionPayment.stylesheet.css";
 
 const SubmissionPayment = () => {
-  return <div>SubmissionPayment</div>;
+  return (
+    <div className="submission__payment__container">
+      <h1>Select Payment Method</h1>
+      <div className="payment__providers">
+        <div className="payment__box">
+          <div className="payment__option paypal">
+            <img src="/src/assets/images/payment/paypal.png" />
+          </div>
+          <h1>Paypal</h1>
+        </div>
+        <div className="payment__box">
+          <div className="payment__option stripe">
+            <img src="/src/assets/images/payment/stripe.png" />
+          </div>
+          <h1>Stripe</h1>
+        </div>
+        <div className="payment__box">
+          <div className="payment__option paycard">
+            <img src="/src/assets/images/payment/securepaycard.png" />
+          </div>
+          <h1>Securepaycard</h1>
+        </div>
+        <div className="payment__box">
+          <div className="payment__option elegro">
+            <img src="/src/assets/images/payment/elegro.png" />
+          </div>
+          <h1>Elegro Acceptance</h1>
+        </div>
+      </div>
+      <div className="submission__agreement">
+        <input type="checkbox" className="agreement__checkbox" />
+        <label className="terms__policy">
+          <strong className="submission__bold__text">
+            I have read and accept the{" "}
+          </strong>
+          Terms and Privacy policy
+        </label>
+      </div>
+      <div className="submission__submit__btn">
+        <button>Submit</button>
+      </div>
+    </div>
+  );
 };
 
 export default SubmissionPayment;
