c78c920135e7fb1591150f93d320fdcac6348ef5
Nikolozi <nchavchavadze@dga.gov.ge>
2023-10-08 14:32:27 +0400

    Changed Aside elements order

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import BookingContext from './context/BookingContext';
-import { Outlet } from 'react-router-dom';
-import AsideContext from './context/AsideContext';
-import ProductContext from './context/ProductContext';
-import { GlobalProvider } from './context/global.context';
-import { IdSequenceProvider } from './context/IdSequence.context';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import BookingContext from "./context/BookingContext";
+import { Outlet } from "react-router-dom";
+import AsideContext from "./context/AsideContext";
+import ProductContext from "./context/ProductContext";
+import { GlobalProvider } from "./context/global.context";
+import { IdSequenceProvider } from "./context/IdSequence.context";
+import { useNavigate } from "react-router-dom";
 export default function App() {
   const [asideActive, setAsideActive] = useState(true);
   const [pages, setPages] = useState([
-    'Home',
-    'Nomad Entertainment',
-    'Hotel Highlights in 3D',
-    'World 3D Tour (sightseeing)',
-    'Book Your Hotel',
+    "Home",
+    "Book Your Hotel",
+    "Nomad Entertainment",
+    "Hotel Highlights in 3D",
+    "World 3D Tour (sightseeing)",
   ]);
   const [activePage, setActivePage] = useState(0);
   const [product, setProduct] = useState(null);
@@ -21,14 +21,14 @@ export default function App() {
 
   const [bookingParams, setBookingParams] = useState({
     showHotels: false,
-    location: 'Tbilisi',
+    location: "Tbilisi",
     checkIn: new Date(),
     checkOut: new Date(),
     guests: 2,
   });
 
   useEffect(() => {
-    navigate('/Home');
+    navigate("/Home");
   }, [navigate]);
 
   return (
